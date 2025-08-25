d6575ec68be2a6c869298280ed07c39d4004601c
WANKHEDE Rohan (Rohan) <Rohan.WANKHEDE@accor.com>
2025-08-25 16:11:37 +0700

    Removed a bug

--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -3,12 +3,7 @@ namespace Data.Repositories;
 
 public class RoleRepository : EfBaseRepository<Role, Guid>, IRoleRepository
 {
-    private readonly AppDbContext _db;
-
-    public RoleRepository(AppDbContext db) : base(db)
-    {
-        _db = db;
-    }
+    public RoleRepository(AppDbContext db) : base(db) { }
 
     public async Task<IReadOnlyList<Role>> ListRolesAsync(CancellationToken ct)
         => await _db.Roles
